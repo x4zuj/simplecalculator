1c89f8205dd2a5aa15db7555c5ba5d1028b35589
fxllenfx <44727794+fxllenfx@users.noreply.github.com>
2026-03-24 19:46:02 +0100

    i made it even better
    beep boop

--- calculator-v2.py
+++ calculator-v2.py
@@ -1,11 +1,20 @@
+ex_count = 1
+
 while True: # we keep it running cause you might want to calculate more often
-    print("Hey :)! Wanna calculate two numbers?")
-    
+    if ex_count == 1:
+        ex_count = ex_count + 1
+        print("Hey :)! Wanna calculate two numbers?")
+    else: 
+        print("You again :D! I know what you want to do.")
     def begin_calc(): # function cause its more elegant, returns result string
         try:
             a = float(input("First number to calculate: ")) 
             b = float(input("Second number to calculate: "))
-            # float otherwise cant calc decimals + converts to int
+            # float otherwise cant calc decimals + converts to int*
+            #
+            # *nerdy:
+            # only ints work because int is a valid float
+            # strings aren't valid floats, so entering letters raises ValueError
             
             # request operator
             print("Great! Would you like to use any of the following operators?")
@@ -13,15 +22,16 @@ while True: # we keep it running cause you might want to calculate more often
             
             # same logic begins
             if op == "+":
-                result = a + b # result variable instead of print statements everywhere
+                result = a + b # result var instead of prints everywhere
             elif op == "-":
                 result = a - b
             elif op == "*":
                 result = a * b
             elif op == "/":
-                if a or b == 0: # this prevents having 2 error messages
-                    return "Division by 0 is not allowed >:(" 
-                    #       ^ we get angry and return this string instead
+                if 0 in (a, b): # this prevents having 2 error messages 
+                       # ^ (only {b} is relevant in div but we still check {a} for your education)
+                    return f"{a} divided by {b} is not allowed >:(" 
+                    #      ^ we get angry and return this string instead
                 result = a / b
             else:
                 return "That isn't an operator >:(" # we get angry again
